7cdf83967797a1d5f99232d069f41b9458a078cf
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2023-01-12 17:28:53 +0700

    create login & register page

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Register from './Pages/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/Register.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='formContainer'>
+        <div className='formWrapper'>
+        <span className='logo'>Chat Together</span>
+        <span className='title'>Register</span>
+            <form>
+                <input type="text" placeholder="display name" />
+                <input type="email" placeholder="email" />
+                <input type="password" placeholder="password" />
+                <input type="file"/>
+                <button>Sign up</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Register
